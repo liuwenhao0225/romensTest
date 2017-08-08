442b396387b1ebd0585f7e389f0dd253e607017d
liuwenhao <617554087@qq.com>
2017-08-08 18:08:14 +0800

    test check

---
+++ test/test.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>测试zend studio专用文件</title>
+</head>
+<body>
+    <p>Test zend studio running status</p>
+</body>
+<script type="text/javascript">
+	console.log(111);
+	alert(333);
+</script>
+</html>
